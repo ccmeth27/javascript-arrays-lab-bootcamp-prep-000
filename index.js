4de3a9019f44a2d6118aa22d40dea3da7191f576
Connor Meth <connormeth27@gmail.com>
2019-08-15 16:23:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(array, element) {
-  var newKittens = kittens.unshift("Ralph");
+  var newKittens = kittens.push("Ralph");
   return newKittens;
 }
 function destructivelyPrependKitten(array, element) {
-  var newCats = kittens.push("Bob");
+  var newCats = kittens.unshift("Bob");
   return newCats;
 }
